19a1bc50131c63d1f14be980c9d40d2e0f500f16
emcyroyale <emcyflorida@gmail.com>
2018-06-29 11:00:45 -0400

    Header updates

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -2,6 +2,30 @@
 #include <fstream>
 #include <iostream>
 
+void Chip8::Initialize()
+{
+    pc = 0x200;    //Start Address
+    opcode = 0;
+    i_reg = 0;
+    sp = 0;
+
+	for(int i = 0; i < 2048; ++i)
+		display[i] = 0;
+
+	// Clear stack
+	for(int i = 0; i < 16; ++i)
+		stack[i] = 0;
+
+	for(int i = 0; i < 16; ++i)
+		key_code[i] = V[i] = 0;
+
+	// Clear memory
+	for(int i = 0; i < 4096; ++i)
+		memory[i] = 0;
+					
+    //Clear screen
+    drawFlag = true;
+}
 
 void Chip8::EmulateCycle()
 {
@@ -10,6 +34,8 @@ void Chip8::EmulateCycle()
 
 bool Chip8::LoadApplication(const char * romname)
 {
+    Initialize();
+
     std::ifstream ifs (romname, std::ifstream::in);
     
     while(ifs.good())
@@ -17,6 +43,11 @@ bool Chip8::LoadApplication(const char * romname)
         std::cout << ifs.get();
     }
 
+    if (ifs.is_open())
+    {
+        std::cout << "Yo!\n";
+
+    }
     //Read into buffer
     //Read into memory
 
--- src/chip8.h
+++ src/chip8.h
@@ -4,15 +4,29 @@
 
 class Chip8
 {
-    private:
-    
-    unsigned char memory[4096]; 
-    
-
     public:
+
+    bool drawFlag;
+
+    unsigned char display[64 * 32];
+    unsigned char key_code[16];
 
     void EmulateCycle();
     bool LoadApplication(const char * romname); 
+
+    private:
+    
+    unsigned short pc;
+    unsigned short opcode;
+    unsigned short i_reg;
+    unsigned short sp;
+
+    unsigned char V[16];
+    unsigned short stack[16];
+    unsigned char memory[4096]; 
+    
+    void Initialize();
+
 
 };
 #endif
